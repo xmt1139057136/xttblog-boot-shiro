1fb4ce6ffe09f1bca1f7f4f93d45f9bc3e78e472
herman.xiong <herman.xiong@caidao1.com>
2019-12-21 09:31:36 +0800

    业余草SpringBoot 整合 Shiro

---
+++ src/main/java/com/xttblog/config/ShiroConfig.java
@@ -0,0 +1,32 @@
+package com.xttblog.config;
+
+import com.xttblog.shiro.MyRealm;
+import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
+import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ShiroConfig {
+    @Bean
+    MyRealm myRealm() {
+        return new MyRealm();
+    }
+    @Bean
+    DefaultWebSecurityManager securityManager() {
+        DefaultWebSecurityManager manager =
+                new DefaultWebSecurityManager();
+        manager.setRealm(myRealm());
+        return manager;
+    }
+    @Bean
+    ShiroFilterChainDefinition shiroFilterChainDefinition() {
+        DefaultShiroFilterChainDefinition definition =
+                new DefaultShiroFilterChainDefinition();
+        definition.addPathDefinition("/doLogin", "anon");
+        definition.addPathDefinition("/**", "authc");
+        return definition;
+    }
+}
+
